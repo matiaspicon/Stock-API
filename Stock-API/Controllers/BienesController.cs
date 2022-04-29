4d6e46b545afeb91745f18b8a728067137708cb9
Matias Picon <mpicon@ssn.gob.ar>
2022-04-29 14:06:45 -0300

    modified valores por defecto bien no encontrado and id to string

--- Stock-API/Controllers/BienesController.cs
+++ Stock-API/Controllers/BienesController.cs
@@ -12,43 +12,27 @@ namespace Stock_API.Controllers
     {
         public BienPatrimonio Get(String id)
         {
-            //peticion a base de datos SP: [dbo].[SAF_BIENPATRIMONIO_GetById]
-           
+            //peticion a base de datos, ejecuta SP: [dbo].[SAF_BIENPATRIMONIO_GetById]           
             ObjectResult<SAF_BIENPATRIMONIO_GetById_Result> bienEncontrado = new SAFEntities().SAF_BIENPATRIMONIO_GetById(int.Parse(id), null);
+            var bienesArray = bienEncontrado.ToArray();
 
-            var bien = bienEncontrado.ToArray()[0];
-
-            var bienConvertido = new BienPatrimonio()
+            //setear valores por defecto si no se encuentra
+            BienPatrimonio bienConvertido = new BienPatrimonio()
             {
-                IdBienPatrimonio = bien.IdBienPatrimonio,
-                IdResponsable = bien.IdResponsable,
-                IdSecretariaGeneral = bien.IdSecretariaGeneral,
-                IdUnidad = bien.IdUnidad,
-                IdUsuarioACargo = bien.IdUsuarioACargo,
-                PatCantidad = bien.PatCantidad,
-                Clasificacion = bien.Clasificacion,
-                Nombre = bien.nombre,
-                PatDescrip = bien.PatDescrip,
-                PatFoto = bien.PatFoto,
-                PatUbicacion = bien.PatUbicacion
+                IdBienPatrimonio = "0",
+                IdResponsable = 0,
+                PatDescrip = "Bien no encontrado"
             };
-            return bienConvertido;
-        }
 
-
-        public List<BienPatrimonio> Get([FromBody] List<String> ids)
-        {
-            List<BienPatrimonio> bienes = new List<BienPatrimonio>();
-
-            foreach (var id in ids)
+            if (bienesArray.Length > 0)
             {
-                ObjectResult<SAF_BIENPATRIMONIO_GetById_Result> bienEncontrado = new SAFEntities().SAF_BIENPATRIMONIO_GetById(int.Parse(id), null);
+                //guarda el primer resultado
+                var bien = bienesArray[0];
 
-                var bien = bienEncontrado.ToArray()[0];
-
-                bienes.Add(new BienPatrimonio()
+                //crea el objeto para la respuesta
+                bienConvertido = new BienPatrimonio()
                 {
-                    IdBienPatrimonio = bien.IdBienPatrimonio,
+                    IdBienPatrimonio = bien.IdBienPatrimonio.ToString(),
                     IdResponsable = bien.IdResponsable,
                     IdSecretariaGeneral = bien.IdSecretariaGeneral,
                     IdUnidad = bien.IdUnidad,
@@ -58,10 +42,53 @@ namespace Stock_API.Controllers
                     Nombre = bien.nombre,
                     PatDescrip = bien.PatDescrip,
                     PatFoto = bien.PatFoto,
-                    PatUbicacion = bien.PatUbicacion                    
-                });
+                    PatUbicacion = bien.PatUbicacion
+                };
             }
+            //retorna el valor buscado
+            return bienConvertido;
+        }
 
+
+        public List<BienPatrimonio> Post([FromBody] List<String> ids)
+        {
+            List<BienPatrimonio> bienes = new List<BienPatrimonio>();
+
+            foreach (var id in ids)
+            {
+                ObjectResult<SAF_BIENPATRIMONIO_GetById_Result> bienEncontrado = new SAFEntities().SAF_BIENPATRIMONIO_GetById(int.Parse(id), null);
+                               
+                var bienesArray = bienEncontrado.ToArray();
+
+                if (bienesArray.Length > 0)
+                {
+                    var bien = bienesArray[0];
+
+                    bienes.Add(new BienPatrimonio()
+                    {
+                        IdBienPatrimonio = bien.IdBienPatrimonio.ToString(),
+                        IdResponsable = bien.IdResponsable,
+                        IdSecretariaGeneral = bien.IdSecretariaGeneral,
+                        IdUnidad = bien.IdUnidad,
+                        IdUsuarioACargo = bien.IdUsuarioACargo,
+                        PatCantidad = bien.PatCantidad,
+                        Clasificacion = bien.Clasificacion,
+                        Nombre = bien.nombre,
+                        PatDescrip = bien.PatDescrip,
+                        PatFoto = bien.PatFoto,
+                        PatUbicacion = bien.PatUbicacion
+                    });
+                }
+                else
+                {
+                    //devolver objeto vacio
+                    bienes.Add(new BienPatrimonio()
+                    {
+                        IdBienPatrimonio = id,
+                        PatDescrip = "Bien no encontrado"
+                    });
+                }
+            }
 
             return bienes;
         }
